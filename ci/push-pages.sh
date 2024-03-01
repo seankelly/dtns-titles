63e6b62388a92138a576024d1867c7a801bd6852
Sean Kelly <code@katron.org>
2024-02-29 19:22:22 -0500

    Commit updated results

--- ci/push-pages.sh
+++ ci/push-pages.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 set -eu
+
+git switch gh-pages
+cp -a pages/*
+rm -r pages
+
+git config --global user.name 'GitHub Actions'
+git config --global user.email 'seankelly@users.noreply.github.com'
+git add .
+if git diff --name-only | grep -q . ; then
+    git commit --message "Automatic title update"
+    git push
+fi
