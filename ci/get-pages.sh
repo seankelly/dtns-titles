05209057f9107e677cc6f03f3c321c3c1ce885e9
Sean Kelly <code@katron.org>
2024-02-29 19:18:15 -0500

    Use remote name to get titles.json from gh-pages

--- ci/get-pages.sh
+++ ci/get-pages.sh
@@ -6,5 +6,5 @@ mkdir pages
 
 git remote update
 
-git show gh-pages:titles.json > pages/titles.json
+git show origin/gh-pages:titles.json > pages/titles.json
 cp -a index.html search-titles.js pages
