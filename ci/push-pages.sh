0181f704173e2f2cd361694dc852d5bd0c13b31d
Sean Kelly <code@katron.org>
2024-02-29 20:56:31 -0500

    Run git add after checking git-diff
    Adding the files means git diff won't return anything because any
    changes have already been staged.

--- ci/push-pages.sh
+++ ci/push-pages.sh
@@ -8,10 +8,8 @@ rm -r pages
 
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'seankelly@users.noreply.github.com'
-ls -l *
-git status
-git add .
-if git diff --name-status | grep -q -E '^M' ; then
+if ! git diff-index --quiet HEAD ; then
+    git add .
     git commit --message "Automatic title update"
     git push
 fi
