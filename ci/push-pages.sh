f4e996dee3c1d7114e56a961794ebf15ed306c82
Sean Kelly <code@katron.org>
2024-03-01 21:17:40 -0500

    Enable some debug output

--- ci/push-pages.sh
+++ ci/push-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 git switch gh-pages
 cp -a pages/* .
@@ -8,6 +8,7 @@ rm -r pages
 
 git config --global user.name 'GitHub Actions'
 git config --global user.email 'seankelly@users.noreply.github.com'
+git diff-index HEAD
 if ! git diff-index --quiet HEAD ; then
     git add .
     git commit --message "Automatic title update"
